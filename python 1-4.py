5b4af497c8f8ec50d536e44e90f6f0e98ac42452
Chris7val <chris7valdez2001@gmail.com>
2022-07-11 21:35:32 -0400

    pythoncode

---
+++ python 1-4.py
@@ -0,0 +1,148 @@
+
+#x = "chris".title()
+#print(f"Hello { x }, would you like to learn some Pyhton today?")
+#y = "  chris"
+#print(y.upper())
+#print(y.lower())
+#print(y.title())
+#print("Please enter the famous person")
+#famousPerson = input()
+#print("Enter the Quote")
+#quote = input()
+#print(f" { famousPerson } once said , {quote} .")
+#print(y)
+#print(y.lstrip())
+#print(y.rstrip())
+#print(y.strip())
+#print(5+3)
+#print(2*4)
+#print(16/2)
+#print(9-1)
+#favorite_num = 7
+#print(f"{favorite_num} is my favorite number.")
+#Names = ['Ash', 'chris', 'niko']
+#print(Names[0].title())
+#print(Names[1].title())
+#print(Names[2].title())
+#print(f"Hello {Names[0].title()} nice to meet you.")
+#print(f"Hello {Names[1].title()} nice to meet you.")
+#print(f"Hello {Names[2].title()} nice to meet you.")
+#vehicles = ['honda', 'bmw', 'toyota', 'mercedes']
+#print(f"I would love to own a {vehicles[-2]} .")
+#print(f"Owning a {vehicles[1]} is too expensive")
+#guest_list = ['ash', 'chris', 'gorge']
+#print(f"{guest_list[0]}, you are invited to my sweet sixteen")
+#print(f"{guest_list[1]}, you are invited to my sweet sixteen")
+#print(f"{guest_list[2]}, you are invited to my sweet sixteen")
+#print("Who cant make it to the part")
+#not_going = input()
+#guest_list.remove(not_going)
+#print(f"{not_going.title()} cant make it to the party")
+#print(guest_list)
+#print("new person to invite")
+#new_invite = input()
+#guest_list.append(new_invite) #.append adds to the end of a list .insert lets you add it too where ever element you want
+#print(guest_list)
+#print(f"{guest_list[-1]} , you are invited to my sweet sixteen""\n")
+#guest_list.insert(0, "frog")
+#guest_list.insert(2, "jake")
+#guest_list.append("fin")
+#print(f"{guest_list[0].title()}, you are invited to my sweet sixteen")
+#print(f"{guest_list[1].title()}, you are invited to my sweet sixteen")
+#print(f"{guest_list[2].title()}, you are invited to my sweet sixteen")
+#print(f"{guest_list[3].title()}, you are invited to my sweet sixteen")
+#print(f"{guest_list[4].title()}, you are invited to my sweet sixteen")
+#print(f"{guest_list[5].title()}, you are invited to my sweet sixteen")
+#print(guest_list)
+#print("You can only invite two people for dinner")
+#cant_come = guest_list.pop()
+#print(f"sorry {cant_come} i cant invite you to dinner")
+#cant_come = guest_list.pop()
+#print(f"sorry {cant_come} i cant invite you to dinner")
+#cant_come = guest_list.pop()
+#print(f"sorry {cant_come} i cant invite you to dinner")
+#cant_come = guest_list.pop()
+#print(f"sorry {cant_come} i cant invite you to dinner")
+#print(f"{guest_list[0].title()}, you are still invited to my sweet sixteen")
+#rint(f"{guest_list[1].title()}, you are still invited to my sweet sixteen")
+#print(guest_list)
+#del guest_list[0]
+#del guest_list[0]
+#print(guest_list)
+
+#locations_to_visit = ['england', 'cali', 'hawaii', 'mexico', 'france']
+#print(locations_to_visit)
+#print(sorted(locations_to_visit))
+#print(locations_to_visit)
+#locations_to_visit.reverse()
+#print(locations_to_visit)
+#locations_to_visit.reverse()
+#print(locations_to_visit)
+#locations_to_visit.sort()
+#print(locations_to_visit)
+#locations_to_visit.sort(reverse=True)
+#print(locations_to_visit)
+#print(len(locations_to_visit))
+#for locations in locations_to_visit:
+#    print(locations)
+# a for loop
+# pizzas = ['pepperoni', 'cheese', 'pineapple']
+# for pizza in pizzas:
+#     print(f"I LIKE {pizza.upper()} PIZZA""\n")
+# print("I REALLY LOVE PIZZA!")
+# animals = ['dog', 'cat', 'fish']
+# for animal in animals:
+#     print(f"A {animal.upper()} would make a great pet")
+# print("Any of these animals would make a great pet!")
+#
+# for number in range(1, 21):
+#     print(number)
+#
+# number = list(range(1, 1000001))
+# for n in number:
+#     print(n)
+# print(f"the max is {max(number)}")
+# print(f"the min is {min(number)}")
+# print(f"the sum is {sum(number)}")
+# odd = list(range(1, 21, 2))
+# for value in odd:
+#     print(value)
+# threes = []
+# for value in range(3, 31, 3):
+#     threes.append(value)
+# for n in threes:
+#     print(n)
+# cube = []
+# for value in range(1, 11):
+#     cube.append(value ** 3)
+# for n in cube:
+#     print(n)
+#comperhension list
+# cubes = [value**3 for value in range(1, 11)]
+# print(cubes)
+# artist = ['banks', 'cloud', 'mike', 'black', 'leo']
+# print("The first Three artist are:")
+# print(artist[0:3])
+# print("The middle Three artist are:")
+# print(artist[1:4])
+# print("The last Three artist are:")
+# print(artist[-3:])
+# pizzas = ['pepperoni', 'cheese', 'pineapple',]
+# friend = pizzas[:]
+# pizzas.append('meat')
+# friend.append('bacon')
+# print("my favorite pizza are:")
+# for my in pizzas:
+#     print(my.title())
+# print("my friends favorite pizza are:")
+# for fr in friend:
+#     print(fr.title())
+# buffet_food = ('rice', 'chicken', 'coconut shrimp', 'meat', 'fries')
+# for foods in buffet_food:
+#     print(foods.title())
+# print("\nnew menu")
+# buffet_food = ('rice', 'chicken', 'coconut shrimp', 'cow', 'brains')
+# for newfood in buffet_food:
+#     print(newfood.title())
+
+
